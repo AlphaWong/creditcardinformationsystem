397653c52616e9b94dc767aff1ddd96f9f4509cf
Alpha WONG <jhexe2002@gmail.com>
2015-04-02 21:19:35 +0800

    <Development> 1. Offered a logger function of the express.

--- index.js
+++ index.js
@@ -1,37 +1,41 @@
 'use strict';
 var http = require('http'),
-  fs = require('fs'),
-  express = require('express'),
-  jade = require('jade'),
-  bodyParser = require('body-parser'),
-  session = require('express-session'),
-  numCPUs = require('os').cpus().length,
-  cwd = process.cwd(),
-  expire = 60 * 1000 * 60 * 24 * 7;
+    fs = require('fs'),
+    express = require('express'),
+    jade = require('jade'),
+    bodyParser = require('body-parser'),
+    session = require('express-session'),
+    morgan = require('morgan'),
+    numCPUs = require('os').cpus().length,
+    cwd = process.cwd(),
+    expire = 60 * 1000 * 60 * 24 * 7;
+
+
 
 var app = express();
 
+app.use(morgan('combined'));
 app.use(require('compression')());
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 app.use(bodyParser.json());
 app.use(require('method-override')());
 app.use(require('cookie-parser')('CreditCardinFormationSystem'));
 
 app.use('/', express.static(cwd + '/public', {
-  maxAge: expire
+    maxAge: expire
 }));
 
 app.set('view engine', 'jade');
 app.engine('jade', require('jade').__express);
 
-app.get('/*', function(req, res) {
-  res.redirect('index.html');
+app.get('/*', function (req, res) {
+    res.redirect('index.html');
 });
 
 var httpServ = http.Server(app),
-  server_port = process.env.PORT || process.env.SERVER_PORT || 80,
-  server_ip_address = process.env.IP || process.env.SERVER_IP || '0.0.0.0';
+    server_port = process.env.PORT || process.env.SERVER_PORT || 80,
+    server_ip_address = process.env.IP || process.env.SERVER_IP || '0.0.0.0';
 
 httpServ.listen(server_port, server_ip_address);
