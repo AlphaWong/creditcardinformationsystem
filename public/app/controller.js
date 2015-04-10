268f861626ef78dee986f24843ed3c0c186c11a7
Eric Ng <eric4545@gmail.com>
2015-04-10 23:04:28 +0800

    UI: trying responsive post item

--- public/app/controller.js
+++ public/app/controller.js
@@ -1,16 +1,10 @@
 'use strict';
 
 app.controller('PostCtrl', function($scope, Post) {
-        $scope.init = function() {
-
-        };
-        $scope.test = function() {
-            console.log(Post.query());
-        };
-    })
-    .config(function($mdThemingProvider) {
-        // Configure a dark theme with primary foreground yellow
-        $mdThemingProvider.theme('docs-dark', 'default')
-            .primaryPalette('yellow')
-            .dark();
-    });
+  $scope.init = function() {
+    $scope.posts = Post.query();
+  };
+  $scope.test = function() {
+    console.log(Post.query());
+  };
+})
